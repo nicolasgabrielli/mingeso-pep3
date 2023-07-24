18dfa36a5bd225811ebf60d8ca088afdc05f1f00
nicolasgabrielli <nicolas.gabrielli@usach.cl>
2023-07-24 18:21:34 -0400

    Se agregaron los servicios

---
+++ easy/src/main/java/cl/mingeso/easy/EasyApplication.java
@@ -0,0 +1,13 @@
+package cl.mingeso.easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyApplication.class, args);
+	}
+
+}
---
+++ easy/src/test/java/cl/mingeso/easy/EasyApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mingeso.easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
